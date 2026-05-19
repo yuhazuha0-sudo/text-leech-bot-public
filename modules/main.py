3113b9a9b3d071cec2973d78783651445ba0c24e
yuhazuha0-sudo <yuhazuha0@gmail.com>
2026-05-19 08:39:17 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -167,6 +167,7 @@ async def account_login(bot: Client, m: Message):
              url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+            name1 = re.sub(r'[^a-zA-Z0-9 _.-]', '', name1)
             name = f'{str(count).zfill(3)}) {name1[:60]}'
 
             if "youtu" in url:
