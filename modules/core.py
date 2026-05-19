756bd753750d940552586fe9a8e7c17f20dd8e11
yuhazuha0-sudo <yuhazuha0@gmail.com>
2026-05-19 08:10:36 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -18,13 +18,25 @@ from pyrogram.types import Message
 
 
 def duration(filename):
-    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                             "format=duration", "-of",
-                             "default=noprint_wrappers=1:nokey=1", filename],
+    result = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            filename
+        ],
         stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-    return float(result.stdout)
-    
+        stderr=subprocess.STDOUT
+    )
+
+    try:
+        return float(result.stdout.decode().strip())
+    except:
+        return 0    
 def exec(cmd):
         process = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         output = process.stdout.decode()
