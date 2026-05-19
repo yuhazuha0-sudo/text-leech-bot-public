a59eb5ff3a1205c978d63dc3d9c8fe655a71eb97
yuhazuha0-sudo <yuhazuha0@gmail.com>
2026-05-19 09:01:19 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -205,7 +205,7 @@ async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
 
 
 async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
-    subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
+    subprocess.run(f'ffmpeg -y -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
     reply = await m.reply_text(f"**⥣ Uploading ...** » `{name}`")
     try:
